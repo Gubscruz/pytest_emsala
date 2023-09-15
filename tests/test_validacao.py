7915cd734cd54595f0215009999e0008cd1a3907
Gubscruz <124577208+Gubscruz@users.noreply.github.com>
2023-09-15 18:55:36 -0300

    pytest

--- tests/test_validacao.py
+++ tests/test_validacao.py
@@ -1,7 +1,29 @@
 import pytest
 from ..validacao import valida_cpf, valida_idade
 
-@pytest.mark.entrada_de_dados
+
+@pytest.mark.cpf
 def test_valida_cpf_valido():
+    # * passa um input valido e espera que retorne true
     assert valida_cpf("33652827610") == True
 
+@pytest.mark.cpf
+def test_valida_cpf_mais_de_11_dig():
+    assert valida_cpf("132074234297364") == False
+
+@pytest.mark.cpf
+def test_valida_cpf_menos_de_11_dig():
+    assert valida_cpf("3189276") == False
+
+@pytest.mark.cpf
+def test_valida_cpf_somente_numeros():
+    assert valida_cpf("abcde123456") == False
+
+@pytest.mark.cpf
+def test_valida_cpf_somente_strings():
+    assert valida_cpf(12345678901) == False
+
+
+@pytest.mark.idade
+def test_valida_idade_positiva():
+    assert valida_idade(-5) == False
--- validacao.py
+++ validacao.py
@@ -1,6 +1,15 @@
 def valida_cpf(cpf: str) -> bool:
-    pass
+
+    if not isinstance(cpf, str):
+        return False
+
+    if len(cpf) != 11:
+        return False
+    
+    if not cpf.isdigit():
+        return False
+
+    return True
 
 def valida_idade(idade: int) -> bool:
     pass
-
